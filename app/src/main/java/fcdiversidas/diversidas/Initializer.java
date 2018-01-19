06dc52744fe474750812f0e34411ff29d7332791
Zachary Bys <zachary.bys@gmail.com>
2018-01-19 22:49:59 +0100

    added full images and upload to server

---
+++ app/src/main/java/fcdiversidas/diversidas/Initializer.java
@@ -0,0 +1,40 @@
+package fcdiversidas.diversidas;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import net.gotev.uploadservice.BinaryUploadRequest;
+import net.gotev.uploadservice.UploadService;
+import net.gotev.uploadservice.okhttp.OkHttpStack;
+
+import java.io.File;
+
+/**
+ * Created by Zachary Bys on 2018-01-19.
+ */
+
+public class Initializer extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        UploadService.NAMESPACE = BuildConfig.APPLICATION_ID;
+
+        UploadService.HTTP_STACK = new OkHttpStack(); // a new client will be automatically created
+    }
+
+    public void uploadBinary(final Context context, String filePath, String fileName) {
+        try {
+            // starting from 3.1+, you can also use content:// URI string instead of absolute file
+            String uploadId =
+                    new BinaryUploadRequest(context, "http://ec2-54-236-246-164.compute-1.amazonaws.com:3000/test")
+                            .setFileToUpload(filePath)
+                            .addHeader(fileName, new File(filePath).getName())
+                            .setMaxRetries(2)
+                            .startUpload();
+        } catch (Exception exc) {
+            Log.e("upload",exc.getMessage());
+        }
+    }
+
+}
--- app/src/main/java/fcdiversidas/diversidas/TimelinePage.java
+++ app/src/main/java/fcdiversidas/diversidas/TimelinePage.java
@@ -2,19 +2,33 @@ package fcdiversidas.diversidas;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import net.gotev.uploadservice.UploadService;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TimelinePage extends AppCompatActivity {
 
+    Initializer initializer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline_page);
+
+        initializer = new Initializer();
 
         Button reactionButton = (Button) findViewById(R.id.photobutton);
         reactionButton.setOnClickListener(new Button.OnClickListener() {
@@ -22,25 +36,84 @@ public class TimelinePage extends AppCompatActivity {
                 dispatchTakePictureIntent();
             }
         });
+
+        Button displayButton = (Button) findViewById(R.id.displaybutton);
+        displayButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                setPic();
+            }
+        });
     }
 
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView imageView = (ImageView)  findViewById(R.id.imageView);
-            imageView.setImageBitmap(imageBitmap);
-        }
+    private void setPic() {
+        // Get the dimensions of the View
+
+        ImageView imageView = (ImageView)  findViewById(R.id.imageView);
+
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+        initializer.uploadBinary(this,mCurrentPhotoPath,imageFileName);
+
+        imageView.setImageBitmap(bitmap);
+    }
+
+    String mCurrentPhotoPath;
+    String imageFileName;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
